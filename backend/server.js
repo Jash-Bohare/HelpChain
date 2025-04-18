f23bc1caafa0131b512b1228f562ea9e67ec90ad
Jash-Bohare <jashbohare@gmail.com>
2025-04-18 20:05:39 +0530

    changes env var

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import walletRoutes from "./walletRoutes.js"; 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
